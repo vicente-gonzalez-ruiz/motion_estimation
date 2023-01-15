d4feaaa8ccb34dbdd402c2d3b847339f8863d9eb
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2023-01-15 14:17:22 +0100

    .

--- src/motion_estimation/optical_flow.py
+++ src/motion_estimation/optical_flow.py
@@ -1,3 +1,12 @@
+import logging
+logger = logging.getLogger(__name__)
+#logging.basicConfig(format="[%(filename)s:%(lineno)s %(funcName)s()] %(message)s")
+#logger.setLevel(logging.CRITICAL)
+#logger.setLevel(logging.ERROR)
+logger.setLevel(logging.WARNING)
+#logger.setLevel(logging.INFO)
+#logger.setLevel(logging.DEBUG)
+
 import cv2
 import numpy as np
 
