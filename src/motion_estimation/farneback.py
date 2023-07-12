76b8e6dffbbfb5134f0e5f3fa4a23dd59d76c2dc
Vicente González-Ruiz <vruiz@ual.es>
2023-07-12 11:31:30 +0200

    .

--- src/motion_estimation/farneback.py
+++ src/motion_estimation/farneback.py
@@ -21,29 +21,6 @@ POLY_SIGMA = 1.2
 SIGMA = 2.0
 PYR_SCALE = 0.5
 
-def Farneback_ME(predicted:np.ndarray,
-                 reference:np.ndarray,
-                 initial_MVs:np.ndarray=None,
-                 pyr_scale=0.5,
-                 levels:int=OF_LEVELS,
-                 wside:int=OF_WINDOW_SIDE,
-                 iters:int=OF_ITERS,
-                 poly_n:float=POLY_N,
-                 poly_sigma:float=POLY_SIGMA) -> np.ndarray:
-    logger.info(f"estimate: pyr_scale={pyr_scale} levels={levels} wside={wside} iters={iters} poly_n={poly_n} poly_sigma={poly_sigma}")
-    MVs = cv2.calcOpticalFlowFarneback(
-        prev=predicted,
-        next=reference,
-        flow=initial_MVs,
-        pyr_scale=pyr_scale,
-        levels=levels,
-        winsize=wside,
-        iterations=iters,
-        poly_n=5,
-        poly_sigma=1.2,
-        flags=cv2.OPTFLOW_USE_INITIAL_FLOW | cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
-    return MVs
-
 def get_flow(reference, target, prev_flow=None, pyr_scale=0.5, levels=LEVELS, winsize=WINDOW_SIDE, iterations=ITERS, poly_n:float=POLY_N, poly_sigma:float=POLY_SIGMA):
     logger.info(f"pyr_scale={pyr_scale} levels={levels} winsize={winsize} iterations={iters} poly_n={poly_n} poly_sigma={poly_sigma}")
     flow = cv2.calcOpticalFlowFarneback(target, reference, flow=prev_flow, pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags=cv2.OPTFLOW_USE_INITIAL_FLOW)
--- src/motion_estimation/predict.py
+++ src/motion_estimation/predict.py
@@ -1,28 +1,24 @@
 ''' motion_estimation/predict.py '''
 
+import logging
+logger = logging.getLogger(__name__)
+#logging.basicConfig(format="[%(filename)s:%(lineno)s %(funcName)s()] %(message)s")
+#logger.setLevel(logging.CRITICAL)
+#logger.setLevel(logging.ERROR)
+logger.setLevel(logging.WARNING)
+#logger.setLevel(logging.INFO)
+#logger.setLevel(logging.DEBUG)
+
 import cv2
 import numpy as np
 
-# Signal extension mode used in the OFCA. See https://docs.opencv.org/3.4/d2/de8/group__core__array.html
-#ofca_extension_mode = cv2.BORDER_CONSTANT
-#ofca_extension_mode = cv2.BORDER_WRAP
-#ofca_extension_mode = cv2.BORDER_DEFAULT
-extension_mode = cv2.BORDER_REPLICATE
-#ofca_extension_mode = cv2.BORDER_REFLECT
-#ofca_extension_mode = cv2.BORDER_REFLECT_101
-#ofca_extension_mode = cv2.BORDER_TRANSPARENT
-#ofca_extension_mode = cv2.BORDER_REFLECT101
-#ofca_extension_mode = BORDER_ISOLATED
-print("extension mode =", extension_mode)
+EXTENSION_MODE = cv2.BORDER_REPLICATE
 
-def make(reference: np.ndarray, MVs: np.ndarray) -> np.ndarray:
-    height, width = MVs.shape[:2]
+def warp(reference, flow):
+    logger.info(f"reference.shape={reference.shape}")
+    height, width = flow.shape[:2]
     map_x = np.tile(np.arange(width), (height, 1))
     map_y = np.swapaxes(np.tile(np.arange(height), (width, 1)), 0, 1)
-    map_xy = (MVs + np.dstack((map_x, map_y))).astype('float32')
-    #map_xy = (np.rint(MVs) + np.dstack((map_x, map_y)).astype(np.float32)) # OJO RINT
-    return cv2.remap(reference, map_xy, None, interpolation=cv2.INTER_LINEAR, borderMode=extension_mode)
-    #return cv2.remap(reference, map_xy, None, interpolation=cv2.INTER_NEAREST, borderMode=ofca_extension_mode)
-    
-    #return cv2.remap(reference, cv2.convertMaps(map_x, map_y, dstmap1type=cv2.CV_16SC2), interpolation=cv2.INTER_LINEAR, borderMode=ofca_extension_mode)
-    #return cv2.remap(reference, map_x, map_y, interpolation=cv2.INTER_LINEAR, borderMode=ofca_extension_mode)
+    map_xy = (flow + np.dstack((map_x, map_y))).astype('float32')
+    warped_reference = cv2.remap(reference, map_xy, None, interpolation=cv2.INTER_LINEAR, borderMode=EXTENSION_MODE)
+    return warped_reference
