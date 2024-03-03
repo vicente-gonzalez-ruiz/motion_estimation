8343f6af91556a7cb005de9cf9eaa90a6be92528
Vicente González-Ruiz <vruiz@ual.es>
2024-03-03 22:45:27 +0100

    .

--- src/motion_estimation/_2D/helpers.py
+++ src/motion_estimation/_2D/helpers.py
@@ -28,27 +28,6 @@ def show_vectors(flow, dpi=150, title=None):
     plt.title(title, fontsize=10)
     plt.show()
 
-def project(
-        image,
-        flow,
-        interpolation_mode=cv2.INTER_LINEAR,
-        extension_mode=cv2.BORDER_REPLICATE):
-    
-    logger.info(f"reference.shape={image.shape}")
-    
-    height, width = flow.shape[:2]
-    map_x = np.tile(np.arange(width), (height, 1))
-    map_y = np.swapaxes(np.tile(np.arange(height), (width, 1)), 0, 1)
-    map_xy = (flow + np.dstack((map_x, map_y))).astype('float32')
-    projection = cv2.remap(
-        image,
-        map_xy,
-        None,
-        interpolation=interpolation_mode,
-        borderMode=extension_mode)
-    
-    return projection
-
 def add_coordinates(flow, target):
     return flow + np.moveaxis(np.indices(target.shape), 0, -1)
 
---
+++ src/motion_estimation/_2D/project.py
@@ -0,0 +1,36 @@
+import logging
+logger = logging.getLogger(__name__)
+#logging.basicConfig(format="[%(filename)s:%(lineno)s %(funcName)s()] %(message)s")
+#logger.setLevel(logging.CRITICAL)
+#logger.setLevel(logging.ERROR)
+logger.setLevel(logging.WARNING)
+#logger.setLevel(logging.INFO)
+#logger.setLevel(logging.DEBUG)
+
+import cv2
+import numpy as np
+
+def project(
+        image,
+        flow,
+        interpolation_mode=cv2.INTER_LINEAR,
+        extension_mode=cv2.BORDER_REPLICATE):
+    
+    logger.info(f"reference.shape={image.shape}")
+    
+    height, width = flow.shape[:2]
+    map_x = np.tile(np.arange(width), (height, 1))
+    map_y = np.swapaxes(np.tile(np.arange(height), (width, 1)), 0, 1)
+    map_xy = (flow + np.dstack((map_x, map_y))).astype('float32')
+    projection = cv2.remap(
+        image,
+        map_xy,
+        None,
+        interpolation=interpolation_mode,
+        borderMode=extension_mode)
+    
+    return projection
+
+def add_coordinates(flow, target):
+    return flow + np.moveaxis(np.indices(target.shape), 0, -1)
+
