fe3c29e4b08884c8d6bd1d71da5b1c6497218dc0
Vicente Gonzalez-Ruiz <vicente.gonzalez.ruiz@gmail.com>
2025-10-16 19:02:34 +0200

    .

--- src/motion_estimation/_3D/farneback_opticalflow3d.py
+++ src/motion_estimation/_3D/farneback_opticalflow3d.py
@@ -7,21 +7,33 @@ import numpy as np
 import logging
 import inspect
 
-# Polynomial expansion
-SPATIAL_SIDE = 9    # Side of the Gaussian applicability window used
-                    # during the polynomial expansion. Applicability (that is, the relative importance of the points in the neighborhood) size should match the scale of the structures we wnat to estimate orientation for (page 77). However, small applicabilities are more sensitive to noise.
+# Polynomial expansion parameters
+
+GAUSS_SIZE = 9      # Side of the Gaussian applicability window used
+                    # during the polynomial expansion. Applicability
+                    # (that is, the relative importance of the points
+                    # in the neighborhood) size should match the scale
+                    # of the structures we wnat to estimate
+                    # orientation for (page 77). However, small
+                    # applicabilities are more sensitive to noise.
+                    
 SIGMA_K = 0.15      # Scaling factor used to calculate the standard
                     # deviation of the Gaussian applicability. The
                     # formula to calculate the standard deviation is
-                    # sigma = sigma_k*(spatial_side - 1).
+                    # sigma = sigma_k*(gauss_size - 1).
 
-# OF estimation
+# OF estimation parameters
+
 FILTER_TYPE = "box" # Shape of the filer used to average the flow. It
                     # can be "box" or "gaussian".
+
 FILTER_SIZE = 21    # Size of the filter used to average the G and
                     # matrices (see Eqs. 4.7 and 4.27 of the thesis).
+
 PYRAMID_LEVELS = 3  # Number of pyramid layers
+
 ITERATIONS = 5      # Number of iterations at each pyramid level
+
 PYRAMID_SCALE = 0.5
 
 class OF_Estimation():
@@ -53,7 +65,7 @@ class OF_Estimation():
         self,
         target, reference,
         pyramid_levels=PYRAMID_LEVELS,
-        spatial_side=SPATIAL_SIDE,
+        gauss_size=GAUSS_SIZE,
         iterations=ITERATIONS,
         sigma_k=SIGMA_K,
         filter_type=FILTER_TYPE,
@@ -81,7 +93,7 @@ class OF_Estimation():
             iters=iterations,
             num_levels=pyramid_levels,
             scale=PYRAMID_SCALE,
-            spatial_side=spatial_side,
+            gauss_size=gauss_size,
             sigma_k=sigma_k,
             filter_type=filter_type,
             filter_size=filter_size,
