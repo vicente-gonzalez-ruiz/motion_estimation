749fc6278cc4cae9e96c13f2f002cdc364e22e0a
Vicente González Ruiz <vruiz@ual.es>
2025-10-17 13:06:13 +0200

    .

--- src/motion_estimation/_3D/farneback_optical_flow_3D.py
+++ src/motion_estimation/_3D/farneback_optical_flow_3D.py
@@ -82,7 +82,7 @@ class OF_Estimation():
             iters=iterations,
             num_levels=pyramid_levels,
             scale=PYRAMID_SCALE,
-            spatial_side=spatial_side,
+            gauss_size=gauss_size,
             sigma_k=sigma_k,
             filter_type=filter_type,
             filter_size=filter_size,
